8d609b83fe6f8a5ea403c53ab12e7f9833fcfa1b
gitvanya34 <gitvanya34@mail.ru>
2021-04-28 03:19:56 +0300

    Рефаткоринг кода heatуравнения.cs, баг с отрицательной температурой

--- CoRSaD/CalculationStressLibrary/CalculationStressLibrary/HeatEquation.cs
+++ CoRSaD/CalculationStressLibrary/CalculationStressLibrary/HeatEquation.cs
@@ -6,46 +6,26 @@ using System.Threading;
 using System.Threading.Tasks;
 
 namespace CalculationStressLibrary
-{   /*
-   D - коэффициент теплопроводности
-   dx - шаг по координате
-   dy - шаг по координате
-   dz - шаг по координате
-   dt - шаг по времени
-   t - время
-   tmax - конечное время
-   a - начальная точка
-   b - конечная точка
-   N - количество узлов
-   T_Curent - значения температуры на текущем временном слое
-   T_Next - значения температуры на вычисляемом (следующем) временном слое
-   X_Values - значения по оси Ox
-   K_Values - значения переменного коэффициента D * T_Current
-  ////////
-    D - коэффициент теплопроводности
-    dx - шаг по координате
-    dy - шаг по координате
-    dz - шаг по координате
-    dt - шаг по времени
-    t - время
-    tmax - конечное время
-
-    */
+{  
     public class HeatEquation
     {
-        static double D = 0.9,
-                      dx = 1,
+        private static double D = 0.036,//сплавленного материала 
+                      dx = 1,//шаг 
                       dy = 1,
                       dz = 1;
 
         private double t = 0,
-                       tmax = 0.05,
-                       dt = 0.00005* (dx * dx) / 2 *D;//tay
+                       tmax = 0.005,
+                       dt = 0.0005* (dx * dx) / 2 ;//tay
+
+        private double T_start = 20,//температур аокружающей среды
+                       T_laser = 10000;
+                      
 
         private static int
-                   a = 0,
-                   b = 10,
-                   N = (int)(((double)(b - a)) / dx) + 2;
+                       a = 0,
+                       b = 10,//заменить на voxelmaxXYZ
+                       N = (int)(((double)(b - a)) / dx) +2 ;
 
         double[,,] T_Next = new double[N , N , N ];
         double[,,] T_Curent = new double[N , N , N ];//4измерение время
@@ -61,8 +41,6 @@ namespace CalculationStressLibrary
 
         public HeatEquation()
         {
-
-            t = 0;
             // Инициализация массивов в соответсвии с начальными условиями
             for (int k = 0; k < N; k++)
             {
@@ -70,60 +48,42 @@ namespace CalculationStressLibrary
                 {
                     for (int i = 0; i < N; i++)
                     {
-                        T_Curent[i, j, k] = 20;//начальная температура всех 
-                                               //  T_Next[i] = 0;
-                                               //X_Values[i] = i * dx;
-                                               //Y_Values[j] = j * dy;
-                                               //Z_Values[k] = k * dz;
-
-                        //XYZ_Values[i, j, k] = i * dx;
-
-                        //if (X_Values[i] == 0.4)
-                        //{
-                        //    T_Curent[i] = 5;
+                        T_Curent[i, j, k] = T_start;//начальная всех элементов и окружающей среды                                
                      
                         K_Values[i, j, k] = D * T_Curent[i, j, k];
 
-                        if (i == 0 || i == N)
-                            K_Values[i, j, k] = (1 - D) * T_Curent[i, j, k];
-                        if (j == 0 || j == N)
-                            K_Values[i, j, k] = (1 - D) * T_Curent[i, j, k];
-                        if (k == 0 || k == N)
-                            K_Values[i, j, k] = (1 - D) * T_Curent[i, j, k];
+                        //так как у нас вокруг объекта есть пустые площади задаем для них отдельный  коэф теплопроводности (снизу можно сдлеать подложку на которой происходит печать)
+                        if (i == 0 || i == N-1)
+                            K_Values[i, j, k] = (/*1 -*/ D) * T_Curent[i, j, k];
+                        if (j == 0 || j == N-1)
+                            K_Values[i, j, k] = (/*1 -*/ D) * T_Curent[i, j, k];
+                        if (k == 0 || k == N-1)
+                            K_Values[i, j, k] = (/*1 -*/ D) * T_Curent[i, j, k];
                     }
                 }
             }
-            // T_Curent[1, 1, 1] = 5000;
-
-            ////Задача начального источника 
-            //for (int k = 0; k < N; k++)
-            //{
-            //    for (int j = 0; j < N; j++)
-            //    {
-            //        for (int i = 0; i < N; i++)
-            //        {
-
-            //            K_Values[i, j, k] = D/* * T_Curent[i, j, k]*/;
-            //            //if (i==0 || i==N)
-            //        }
-            //    }
-            //}
-
-
-
-           // T_Curent[5, 5, 1] = 10000;
+            
 
 
         }
         public void CalculationHeatEquation()
         {
-            if(t<0.005)
+
+            //Указываем источники их начальную температуру и время из водействия 
+            if(t< tmax)
             {
-                T_Curent[6, 6, 1] = 10000;
-                T_Curent[6, 5, 1] = 10000;
-                T_Curent[5, 6, 1] = 10000;
-                T_Curent[5, 5, 1] = 10000;
+                T_Curent[6, 6, 1] = T_laser;
+                T_Curent[6, 5, 1] = T_laser;
+                T_Curent[5, 6, 1] = T_laser;
+                T_Curent[5, 5, 1] = T_laser;
+
+                T_Curent[1, N - 2, 1] = T_laser;
+                T_Curent[1, 1, 1] = T_laser;
+                T_Curent[N - 2, 1, 1] = T_laser;
+                T_Curent[N - 2, N - 2, 1] = T_laser;
             }
+
+            //Расчет следующего значения температуры в сетке
             for (int k = 1; k < N-1; k++)
             {
                 for (int j = 1; j < N-1; j++)
@@ -133,21 +93,19 @@ namespace CalculationStressLibrary
                         T_Next[i, j, k] = T_Curent[i, j, k] + (dt / (dx * dx)) * (
                                                    K_half_plus_i(i, j, k) * (T_Curent[i + 1, j, k] - T_Curent[i, j, k])
                                                  - K_half_minus_i(i, j, k) * (T_Curent[i, j, k] - T_Curent[i - 1, j, k])
-
-                                                 + K_half_plus_j(i, j, k) * (T_Curent[i, j + 1, k] - T_Curent[i, j, k])
-                                                 - K_half_minus_j(i, j, k) * (T_Curent[i, j, k] - T_Curent[i, j - 1, k])
-
-                                                  + K_half_plus_k(i, j, k) * (T_Curent[i, j, k + 1] - T_Curent[i, j, k])
-                                                 - K_half_minus_k(i, j, k) * (T_Curent[i, j, k] - T_Curent[i, j, k - 1])
-                                                 );
+                                                 +
+                                                 ( K_half_plus_j(i, j, k) * (T_Curent[i, j + 1, k] - T_Curent[i, j, k])
+                                                 - K_half_minus_j(i, j, k) * (T_Curent[i, j, k] - T_Curent[i, j - 1, k]))
+                                                 +
+                                                  ( K_half_plus_k(i, j, k) * (T_Curent[i, j, k + 1] - T_Curent[i, j, k])
+                                                 - K_half_minus_k(i, j, k) * (T_Curent[i, j, k] - T_Curent[i, j, k - 1]))
+                                                                                ) ;
 
                        
                     }
                 }
             }
-
-           // T_Curent[1, 1, 1] = 1000;
-
+            //перезаписываем значения коэфциентов теплопроводности для всей сетки 
             for (int k = 0; k < N; k++)
             {
                 for (int j = 0; j < N; j++)
@@ -155,12 +113,12 @@ namespace CalculationStressLibrary
                     for (int i = 0; i < N; i++)
                     {                    
                         K_Values[i, j, k] = D * T_Curent[i, j, k];
-                        //if (i == 0 || i == N)
-                        //    K_Values[i, j, k] = (1 - D) * T_Curent[i, j, k];
-                        //if (j == 0 || j == N)
-                        //    K_Values[i, j, k] = (1 - D) * T_Curent[i, j, k];
-                        //if (k == 0 || k == N)
-                        //    K_Values[i, j, k] = (1 - D) * T_Curent[i, j, k];
+                        if (i == 0 || i == N-1)
+                            K_Values[i, j, k] = (/*1 -*/ D) * T_Curent[i, j, k];
+                        if (j == 0 || j == N-1)
+                            K_Values[i, j, k] = (/*1 -*/ D) * T_Curent[i, j, k];
+                        if (k == 0 || k == N-1)
+                            K_Values[i, j, k] = (/*1 -*/ D) * T_Curent[i, j, k];
                     }
                 }
             }
@@ -170,6 +128,7 @@ namespace CalculationStressLibrary
            
             // Console.WriteLine(T_Curent[N-1, N-1, N-1]);
         }
+        //TODO: Добавить метод производящий нормализацию для выводо при большой точности 
         public double K_half_plus_i( int i, int j, int k)
         {
             double K_half = 0;
@@ -214,7 +173,7 @@ namespace CalculationStressLibrary
         }
         public double getTemperatyre(int x, int y, int z)
         {
-            return T_Curent[x*1,y*1,z*1];
+            return T_Curent[x,y,z];
         }
     }
    
